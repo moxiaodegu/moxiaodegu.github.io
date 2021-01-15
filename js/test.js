07e7463fcc690ffde125d18eb741f26210a6030a
mayanli <mayanli@58.com>
2021-01-15 18:00:36 +0800

    update

--- js/test.js
+++ js/test.js
@@ -4,20 +4,20 @@
  * @Author: mayanli
  * @Date: 2021-01-06 19:30:51
  * @LastEditors: mayanli
- * @LastEditTime: 2021-01-14 11:23:21
+ * @LastEditTime: 2021-01-15 17:46:08
  */
 
 
 var a = 1;
 console.log(a);
 function test(a) {
-  console.log(a);
-  var a = 123;
-  console.log(a);
-  function a() { }
-  console.log(a);
-  console.log(b);
-  var b = function () { }
-  console.log(b);
+    console.log(a);
+    var a = 123;
+    console.log(a);
+    function a() {}
+    console.log(a);
+    console.log(b);
+    var b = function() {}
+    console.log(b);
 }
 test(2);
