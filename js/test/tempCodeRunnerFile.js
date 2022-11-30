e09f2248c76275dd18af3f3a0ef37981026e5660
mayanli <mayanli@58.com>
2022-11-30 18:25:41 +0800

    uodate

--- js/test/tempCodeRunnerFile.js
+++ js/test/tempCodeRunnerFile.js
@@ -1,6 +1,28 @@
-function *generator() {
-  yield '1111'
+function SuperFun() {
+  this.name = 'lining'
+  // this.setName = ()=>{
+  //   this.name = 'mamma'
+  // }
 }
 
-const gen = generator()
-console.log(gen.next())
+// SuperFun.prototype.setName = ()=>{
+//   this.name = 'mamma'
+// }
+
+function SubFun(){
+  SuperFun.call(this) // 盗用构造函数
+
+  this.setName = ()=>{
+    this.name = 'mamma'
+  }
+}
+SubFun.prototype = new SuperFun() // 原型模式
+
+const a = new SubFun()
+
+console.log(a.name) // lining
+a.setName()
+console.log(a.name) // mamma
+
+const b = new SubFun()
+console.log(b.name) // mamma
