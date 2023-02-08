573af4c799f035f0abbdffe42fda6ca7f30b72b9
mayanli <mayanli@58.com>
2023-02-08 15:29:26 +0800

    update

--- js/test/tempCodeRunnerFile.js
+++ js/test/tempCodeRunnerFile.js
@@ -1,15 +1,20 @@
-const obj = {
-  name:'lllllllll'
-}
-
-const handler = {
-  get(){
-    return 'mmmmm'
+var twoSum = function (nums, target) {
+  // 获取小于值target的数组
+  let retList = []
+  const arr = nums.sort().filter(num => num < target)
+  for (const i = 0; i < arr.length; i++) {
+    for (const j = 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        console.log(i, j)
+        retList = [i, j]
+        return
+      }
+    }
+    return
   }
-}
+  console.log(retList)
+  return 
 
-const proxy = new Proxy(obj,handler)
+};
 
-console.log(proxy.name)
-
-console.log(obj.name)
+console.log(twoSum([2, 7, 11, 15], 9))
